8f1d865c4cb9884e2f975cba2862cd6378f6e839
JoseCarlos <JoseCarlos@192.168.1.6>
2014-03-29 13:39:24 -0500

    Primera Prueba

--- transmetro/public_html/js/transmetro.js
+++ transmetro/public_html/js/transmetro.js
@@ -3,44 +3,4 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-document.addEventListener("deviceready", onDeviceReady, false);
-
-    // PhoneGap esta listo
-    //
-    function onDeviceReady() {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    }
-
-    // onSuccess Geolocation
-    //
-    function onSuccess(position) {
-       
-          var punto = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-                var config = {
-                    zoom:16,
-                    center:punto,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP 
-                };
-         
-                var mapa = new google.maps.Map($('#mapa')[0],config);
-                var marker = {
-                    position: punto,
-                    titulo:"mi ubicación",
-                    map: mapa,
-                    animation: google.maps.Animation.DROP,
-                    draggable:false
-                };
-                var marcador = new google.maps.Marker(marker);
-                marcador.setMap(mapa);
-                $.getJSON('http://servicedatosabiertoscolombia.cloudapp.net/v1/Alcaldia_de_barranquilla/routes?$format=json', {format: "json"}, function(data) { 
-                    alert(data);
-                });
-    }
-
-    // La función 'callback' onError recibe un objeto `PositionError`.
-    //
-    function onError(error) {
-        alert('código: '    + error.code    + '\n' +
-              'mensaje: ' + error.message + '\n');
-    }
 
